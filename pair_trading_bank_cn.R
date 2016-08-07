bf450ac635859a1063a07d8cf52fd53e49f14d35
zhuzhi <zhuzhi90@gmail.com>
2016-08-07 15:37:26 +0800

    bank prim/mod price ratio

--- pair_trading_bank_cn.R
+++ pair_trading_bank_cn.R
@@ -10,6 +10,7 @@
 # market 数据为　市场日　OHLC　数据
 ############################################################################################################
 
+setwd("~/r_idea/")
 
 ############################################################################################################
 # 读入数据及简单合并
@@ -87,22 +88,46 @@ rm(sh601988_market, sh601988_fuquan)
 # 农业银行上市时间最短
 # 这里调用了 tushare 中的通联接口来获得交易日历
 
-library("rPython")
-python.exec("import tushare")
-python.exec("tushare.set_token('fa8d82ec55d910894965982e026b47469a99b4395df2d4ef90c8c0ddd04a298d')")
-python.exec("trade_cal = tushare.Master().TradeCal(exchangeCD='XSHG', beginDate='20110101', endDate='20160731', field='calendarDate,isOpen,prevTradeDate')")
-python.exec("trade_cal.to_csv('./data/trade_cal', index=False)")
+# library("rPython")
+# python.exec("import tushare")
+# python.exec("tushare.set_token('fa8d82ec55d910894965982e026b47469a99b4395df2d4ef90c8c0ddd04a298d')")
+# python.exec("trade_cal = tushare.Master().TradeCal(exchangeCD='XSHG', beginDate='20110101', endDate='20160731', field='calendarDate,isOpen,prevTradeDate')")
+# python.exec("trade_cal.to_csv('./data/trade_cal', index=False)")
+
+stock_list <- c("sh601288", "sh601398", "sh601939", "sh601988")
+for(stock_code in stock_list){
+  
+  stock_data <- get(stock_code)
+  # stock_data <- stock_data[stock_data$date <= end_date & stock_data$date >= start_date,]
+  cat(nrow(stock_data), min(stock_data$date), max(stock_data$date), fill=TRUE)
+  
+}
 
 ############################################################################################################
 # Z-score and ADF-test
 # 模拟时间段
 # choose 2 in 4: 6种
+# 很多时候停牌原因时间太久，没有好的方法查不了了。
 ############################################################################################################
+
+start_date = "2011-01-01"
+end_date = "2013-12-31"
+
+n_window = 20
+n_adf_win = 60
 
 stock_a = sh601288
 stock_b = sh601398
-start_date = "2011-01-01"
-end_date = "2013-12-31"
+
+
+
+date_a <- stock_a$date
+date_b <- stock_b$date
+
+
+setdiff(date_a, date_b)
+setdiff(date_b, date_a)
+
 
 trade_cal = read.table('./data/trade_cal', sep=",", stringsAsFactors = FALSE, header=TRUE)
 
@@ -111,6 +136,96 @@ stock_a <- stock_a[stock_a$date<=end_date & stock_a$date>=start_date, ]
 stock_b <- stock_b[stock_b$date<=end_date & stock_b$date>=start_date, ]
 trade_cal_sub <- trade_cal[trade_cal$calendarDate<=end_date & trade_cal$calendarDate>=start_date, ]
 
-stock_a_merge <- merge(x=stock_a, y = trade_cal_sub, by.x="date", by.y="calendarDate", all.x=TRUE, all.y=TRUE)
-stock_b_merge <- merge(x=stock_b, y = trade_cal_sub, by.x="date", by.y="calendarDate", all.x=TRUE, all.y=TRUE)
+# stock_data in trading days
+stock_data <- merge(x=stock_a, y=stock_b, by.x="date", by.y="date", all.x=TRUE, all.y=TRUE, suffixes = c("_a", "_b"))
+stock_data <- merge(x=stock_data, y =trade_cal_sub, by.x="date", by.y ="calendarDate", all.x=TRUE, all.y=TRUE)
+stock_data <- stock_data[stock_data$isOpen==1,]
+
+n <- nrow(stock_data)
+
+# close and factor filling
+na_filling  <- function(x){
+  for(i in 2:length(x)){
+    if(is.na(x[i])){
+      x[i]<- x[i-1]
+      }
+  }
+  return(x)
+}
+stock_data$close_a <- na_filling(stock_data$close_a)
+stock_data$factor_a <- na_filling(stock_data$factor_a)
+stock_data$close_b <- na_filling(stock_data$close_b)
+stock_data$factor_b <- na_filling(stock_data$factor_b)
+
+
+# price ratio and moving average/sd
+stock_data$price_ratio <- log(stock_data$close_a/stock_data$close_b)
+stock_data$pr_ma <- NA
+stock_data$pr_msd <- NA
+
+for(i in n_window:n){
+  
+  stock_data[i, "pr_ma"] <- mean(stock_data[(i-n_window+1):i, "price_ratio"])
+  stock_data[i, "pr_msd"] <- sd(stock_data[(i-n_window+1):i, "price_ratio"])
+}
+
+# price rtio's z-score
+
+stock_data$pr_z_score <- (stock_data$price_ratio - stock_data$pr_ma)/stock_data$pr_msd
+
+plot(as.Date(stock_data$date),stock_data$pr_z_score, type='l', main="primitive price ratio z-score", xlab="date", ylab="z-score")
+abline(a=2, b=0, col="red", lty=1)
+abline(a=1, b=0, col="red", lty=2)
+
+abline(a=-2, b=0, col="skyblue", lty=1)
+abline(a=-1, b=0, col="skyblue", lty=2)
+
+# modified price ratio
+# 因为要考虑到分红派息的问题
+# 股票分红派息配股的时候，股价会相应下降，这个时候会对“价差”影响较大
+# 不如使用复权价格来看看 price ratio
+# 复权方法:
+#   定点前复权
+#   在交易日 k， 以交易日 k-20 为定锚进行复权。
+
+
+stock_data$mod_price_ratio <- log(stock_data$close_a/stock_data$close_b)
+stock_data$mpr_ma <- NA
+stock_data$mpr_sd <- NA
+
+for(i in n_window:n){
+  
+  fuquan_price_a <- stock_data[i, "close_a"]*stock_data[i, "factor_a"]/stock_data[i-n_window+1, "factor_a"]
+  fuquan_price_b <- stock_data[i, "close_b"]*stock_data[i, "factor_b"]/stock_data[i-n_window+1, "factor_b"]
+  stock_data[i, "mod_price_ratio"] <- log(fuquan_price_a/fuquan_price_b)
+  stock_data[i, "mpr_ma"] <- mean(stock_data[(i-n_window+1):i, "mod_price_ratio"])
+  stock_data[i, "mpr_msd"] <- sd(stock_data[(i-n_window+1):i, "mod_price_ratio"])
+  
+}
+
+stock_data$mpr_z_score <- (stock_data$mod_price_ratio - stock_data$mpr_ma)/stock_data$mpr_msd
+
+plot(as.Date(stock_data$date),stock_data$mpr_z_score, type='l', main="modified price ratio z-score", xlab="date", ylab="z-score")
+abline(a=2, b=0, col="red", lty=1)
+abline(a=1, b=0, col="red", lty=2)
+
+abline(a=-2, b=0, col="skyblue", lty=1)
+abline(a=-1, b=0, col="skyblue", lty=2)
+
+plot(stock_data$pr_z_score, type="l", col="darkred", xlab="", ylab="", main="pr v.s. mpr")
+lines(stock_data$mpr_z_score, type="l", col="darkblue")
+
+abline(a=2, b=0, col="gray60", lty=1)
+abline(a=1, b=0, col="gray60", lty=2)
+
+abline(a=-2, b=0, col="gray60", lty=1)
+abline(a=-1, b=0, col="gray60", lty=2)
+
+# 显然在分红派息配股之后，进出信号短时间内会有所改变
+
+
+
+
+
+
 
